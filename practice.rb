ecccd91cd4ad574cb8c695d498f2a29a22ddd6dc
Marcus  = Soulo <souloinc87@gmail.com>
2018-07-06 04:30:16 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -5,6 +5,6 @@ require 'pry'
 
 def web_page
   html = open('https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=12551')
-  doc = Nokogiri::HTML(html)
+  doc = Nokogiri::HTML(html.read)
   binding.pry
 end
